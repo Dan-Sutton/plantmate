2c184ffaccd931ce36e52de06693e0da4ce7a2dd
Dan <Dan@Dans-Macbook.local>
2022-03-29 19:12:36 +0100

    transferred old project

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Button, View, Text } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.nav}>
+        <Text style={styles.navText}>Plant Mate</Text>
+      </View>
+
+      {/* welcome content */}
+      <View style={styles.content}>
+        {/* Title card */}
+        <View style={styles.textcontainer}>
+          <Text style={styles.welcome}>Welcome to</Text>
+          <Text style={styles.title}>PlantMate</Text>
+        </View>
+        {/* Text Body */}
+        <View>
+          <Text style={styles.text}>
+            A convenient way to track when your plants need watering, so that
+            they never go under-watered again!
+          </Text>
+        </View>
+
+        <View style={styles.buttons}>
+          <View style={styles.login}>
+            <Button
+              title="Login"
+              color="#4D794E"
+              onPress={() => alert("You Clicked Login")}
+            />
+          </View>
+
+          <Button
+            title="Sign Up"
+            color="#F0B440"
+            onPress={() => alert("You Clicked Login")}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +45,50 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#4D794E",
     alignItems: "center",
+  },
+  nav: {
+    backgroundColor: "#F0B440",
+    width: "100%",
+    height: 80,
     justifyContent: "center",
+    alignItems: "center",
+  },
+  navText: {
+    paddingTop: 20,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  content: {
+    marginTop: 30,
+    width: "80%",
+    height: "70%",
+    backgroundColor: "white",
+    borderRadius: 15,
+  },
+  textcontainer: {
+    marginTop: 10,
+    paddingHorizontal: 20,
+  },
+  welcome: {
+    fontSize: 25,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: "bold",
+  },
+  text: {
+    fontSize: 20,
+    marginTop: 30,
+    paddingHorizontal: 20,
+  },
+  buttons: {
+    marginTop: 60,
+    paddingHorizontal: 60,
+    height: 150,
+  },
+  login: {
+    marginBottom: 30,
   },
 });
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # plantmate
-
-test
